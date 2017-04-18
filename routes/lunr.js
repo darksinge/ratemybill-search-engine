8a269fdd37d7947f5a1d38879fa3ddf77eaf7fda
darksinge <cr.blackburn89@gmail.com>
2017-04-18 09:53:11 -0600

    fixed merge conflict issue

--- routes/lunr.js
+++ routes/lunr.js
@@ -10,20 +10,11 @@ var router = express.Router();
 var path = require('path');
 var fs = require('fs');
 var readline = require('readline');
-<<<<<<< HEAD
 var lunr = require('elasticlunr');
-=======
-var lunr = require('lunr');
->>>>>>> ccc8fc35e76cd0d2b6be790c5c1836d6081acf82
 var clc = require('cli-color');
 var indexer = require(path.join(__dirname, '../indexer/index'));
 
-// var documents = [];
-<<<<<<< HEAD
 var index;
-=======
-var idx;
->>>>>>> ccc8fc35e76cd0d2b6be790c5c1836d6081acf82
 var indexDidLoad = false;
 
 router.use(function(req, res, next) {
@@ -40,7 +31,7 @@ router.get('/', function(req, res) {
         error: 'searchTerms is undefined!'
     });
 
-    var results = idx.search(q);
+    var results = index.search(q);
 
     return res.json({
         results: results
@@ -50,7 +41,7 @@ router.get('/', function(req, res) {
 router.use('/:token', function(req, res) {
     var token = req.params.token;
     return res.json({
-        results: idx.search(token)
+        results: index.search(token)
     });
 });
 
@@ -61,11 +52,7 @@ function buildIndex() {
     
     var indexFilePath = path.join(__dirname, '../indexer/prebuild.json');
     if (fs.existsSync(indexFilePath)) {
-<<<<<<< HEAD
         index = indexer.loadIndex(indexFilePath);
-=======
-        idx = indexer.loadIndex(indexFilePath);
->>>>>>> ccc8fc35e76cd0d2b6be790c5c1836d6081acf82
         indexDidLoad = true;
     }
     
@@ -74,30 +61,19 @@ function buildIndex() {
     
     var msgFormat = clc.xterm(40);
     
-    var args = ['-d', documentsPath]
+    var args = ['-d', documentsPath];
     
     console.log(msgFormat.bold('\n**** Rebuilding index ****'));
     const spawn = require('child_process').spawn;
     const proc = spawn('node ' + buildFilePath, args, {shell: true});
     
     proc.stdout.on('data', function(data) {
-        data = data.toString('utf8');
+        data = data.toString();
         if (data.includes('%')) {
-<<<<<<< HEAD
             readline.clearLine(process.stdout, 0);
             readline.cursorTo(process.stdout, 0, null);
             process.stdout.write(msgFormat('progress: '));
             process.stdout.write(msgFormat(data));
-=======
-            if (process.env.NODE_ENV === 'development') {
-                readline.clearLine(process.stdout, 0);
-                readline.cursorTo(process.stdout, 0, null);
-                process.stdout.write(msgFormat('progress: '));
-                process.stdout.write(msgFormat(data));
-            } else {
-                console.log(data);
-            }
->>>>>>> ccc8fc35e76cd0d2b6be790c5c1836d6081acf82
         } else {
             console.log(msgFormat(data));
         }
